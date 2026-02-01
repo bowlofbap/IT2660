42bdf9cdee6dbbbf563f73f39a97a8094b401696
Daniel Yoo <daniel.yoo0523@gmail.com>
2026-02-01 03:12:58 -0500

    finished main method

--- lab1/Main.java
+++ lab1/Main.java
@@ -10,6 +10,23 @@ public class Main {
 
     Lab1 lab = new Lab1();
     System.out.println(lab.increment(1));
+
+    int[] numbers = {5, 9, 3, 12, 7, 3, 11, 5};
+    for (int n: numbers){
+      System.out.print(n + " ");
+    }
+    System.out.println();
+    for (int i = numbers.length - 1; i >= 0; i--) {
+      System.out.print(numbers[i] + " ");
+    }
+    System.out.println(numbers[0] + " " + numbers[numbers.length - 1]);
+    lab.increment(0);
+    lab.max(0, 5);
+    lab.min(0, 5);
+    lab.sum(numbers);
+    lab.average(numbers);
+    lab.max(numbers);
+    lab.min(numbers);
   }
 }     
 
